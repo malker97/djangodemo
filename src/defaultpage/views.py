1425b451691100bfe7eae54cbc3ff6ad27abfc8d
Zhengmao Zhang <malker@qq.com>
2023-03-01 04:03:25 -0800

    update django code

--- src/defaultpage/views.py
+++ src/defaultpage/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+import requests
 # Create your views here.
 def index(request):
+    # use ipinfo.io to get the IP address of the server
+    ipinfo = requests.get('https://ipinfo.io')
+    ipinfo_json = ipinfo.json()
+    ip = ipinfo_json['ip']
     ipaddr = request.META.get('HTTP_X_FORWARDED_FOR', request.META.get('REMOTE_ADDR'))
-    return HttpResponse("Test GCP Cloud Run CD Trigger status from IP: " + ipaddr)
+    return HttpResponse(ipaddr + "<-" + ip)
--- src/polls/views.py
+++ src/polls/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+import requests
 # Create your views here.
 def index(request):
+    # use ipinfo.io to get the IP address of the server
+    ipinfo = requests.get('https://ipinfo.io')
+    ipinfo_json = ipinfo.json()
+    ip = ipinfo_json['ip']
+
     return HttpResponse("Test GCP Cloud Run CD Trigger status")
